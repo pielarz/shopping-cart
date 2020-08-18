f6fb4a9ed0a72d33a5a8ba313942cef03a5bf933
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-18 15:40:41 +0200

    Created products component with simple data in json format

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
-  return (
-    <div class="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>Footer content</footer>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.vehicles,
+      model: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div class="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>Footer content</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
