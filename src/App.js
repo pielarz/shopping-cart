60dba51d5c616e211e11074314fc7db21bbf2532
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-18 14:24:13 +0200

    Added header, main and footer section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div class="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>Footer content</footer>
     </div>
   );
 }
